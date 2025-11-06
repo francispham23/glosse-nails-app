0b1c84e36e9ad8eec6b43ae9a8716520767705ec
Francis Pham <francis.pham.ca@gmail.com>
2025-11-06 13:10:43 -0800

    initial setup Convex Auth

--- README.md
+++ README.md
@@ -48,3 +48,13 @@ Join our community of developers creating universal apps.
 
 - [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
 - [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+
+## Convex Authentication Setup
+
+- [Convex Auth documentation](https://labs.convex.dev/auth/setup/)
+
+```bash
+npm install @convex-dev/auth @auth/core@0.37.0
+npx @convex-dev/auth
+npx expo install expo-secure-store
+```
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,43 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
-const deploymentURL = process.env.EXPO_PUBLIC_CONVEX_URL;
-const convex = new ConvexReactClient(deploymentURL);
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+const secureStorage = {
+  getItem: SecureStore.getItemAsync,
+  setItem: SecureStore.setItemAsync,
+  removeItem: SecureStore.deleteItemAsync,
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ConvexProvider client={convex}>
+    <ConvexAuthProvider
+      client={convex}
+      storage={
+        Platform.OS === "android" || Platform.OS === "ios"
+          ? secureStorage
+          : undefined
+      }
+    >
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -32,6 +48,6 @@ export default function RootLayout() {
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider>
-    </ConvexProvider>
+    </ConvexAuthProvider>
   );
 }
