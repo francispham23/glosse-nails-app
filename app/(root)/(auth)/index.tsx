5d7333ed98fa6382db0f1ee2eb57984c9a8fae48
Francis Pham <francis.pham.ca@gmail.com>
2025-11-24 16:40:02 -0800

    fix build error

--- app/(root)/(auth)/index.tsx
+++ app/(root)/(auth)/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "heroui-native";
 import { Text, View } from "react-native";
 
 import { Container } from "@/components/container";
-import SignIn from "@/components/signin";
+import SignIn from "@/components/sign-in";
 
 export default function Landing() {
 	return (
